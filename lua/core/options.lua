7f2f003b31894b5c4de4461d13c454826b0bd1a4
HeziCyan <hezicyan@gmail.com>
2023-06-01 21:16:52 +0800

    fix: totally unbind vim from system clipboard

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -16,7 +16,7 @@ local function load_options()
 		backupskip = "/tmp/*,$TMPDIR/*,$TMP/*,$TEMP/*,*/shm/*,/private/var/*,.vault.vim",
 		breakat = [[\ \	;:,!?]],
 		breakindentopt = "shift:2,min:20",
-		clipboard = "unnamed",
+		clipboard = "",
 		cmdheight = 2, -- 0, 1, 2
 		cmdwinheight = 5,
 		colorcolumn = "80",
